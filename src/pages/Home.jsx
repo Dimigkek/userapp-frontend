ae1b8b0a912bb504fa377f1a0b1a8a9e099693ce
dimigkek <dimigkek@gmail.com>
2026-02-04 05:53:40 +0200

    Fix Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 export default function Home() {
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold">User Management App</h1>
-            <p className="mt-2 text-gray-600">
-                Simple Spring Boot + React application
+        <div className="p-10">
+            <h1 className="text-4xl font-bold mb-4">User Management App</h1>
+            <p className="text-gray-400 max-w-xl">
+                Simple full-stack application built with Spring Boot (backend) and
+                React + Tailwind (frontend).
             </p>
         </div>
     );
