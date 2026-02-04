327f97bc7aab63ee07e2a51c6897b4400a339308
dimigkek <dimigkek@gmail.com>
2026-02-04 05:52:24 +0200

    Fix Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="bg-gray-800 text-white px-6 py-4 flex gap-6">
+        <nav className="bg-gray-900 text-white px-6 py-4 flex items-center gap-6 shadow">
+            <span className="font-semibold text-lg">UserApp</span>
+
             <Link to="/" className="hover:text-gray-300">
                 Home
             </Link>
+
             <Link to="/users" className="hover:text-gray-300">
-                Display Users
+                Users
             </Link>
-            <Link to="/users/new" className="hover:text-gray-300">
+
+            <Link
+                to="/users/new"
+                className="ml-auto bg-blue-600 px-4 py-2 rounded hover:bg-blue-500"
+            >
                 Register User
             </Link>
         </nav>
